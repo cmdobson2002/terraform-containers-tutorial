980b2612d842ab86bec79291665be238993a7ed6
charlie.dobson <charlie.dobson@blackcape.io>
2023-09-22 15:17:05 -0400

    all demo files

---
+++ main.tf
@@ -0,0 +1,42 @@
+
+# Contains terraform-specific settings, such as required providers to provision infrastructure
+# Each provider takes a source, which is the hostname/provider type in the Terraform Registry
+# So our docker provider is fully: registry.terraform.io/kreuzwerker/docker
+# Version constraint is optional, terraform automatically downloads most recent version
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+# Configures specified provider (docker)
+# You can use multiple provider blocks to manage resources from different providers (AWS, Azure, etc.)
+provider "docker" {
+  host = "unix:///Users/charliedobson/.docker/run/docker.sock"
+}
+
+# Resource blocks define infrastructure components, can be physical or virtual (docker container)
+# Requires two strings before the block: resource type ("docker_image"), and resource name ("nginx")
+# Used to create ID for docker image: docker_image.nginx
+# Can take arguments such as machine sizes, disk image names
+resource "docker_image" "nginx" {
+  name         = "nginx"
+  keep_locally = false
+}
+
+# Sets docker image as the image source for "docker_container" resource
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  # searches for anything prepended with 'var.' in the variables.tf file
+  name  = var.container_name
+  hostname = "terraform-docker"
+
+  # Docker configuration values
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,10 @@
+# Outputs catch values that are generated during the Terraform apply stage and output them to the terminal
+output "container_id" {
+  description = "ID of the Docker container"
+  value = docker_container.nginx.id
+}
+
+output "image_id" {
+  description = "ID of the Docker image"
+  value = docker_image.nginx.id
+}
