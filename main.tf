586f443f5281fbd16110c343cec5f5dbe044e86b
charlie.dobson <charlie.dobson@blackcape.io>
2023-09-23 14:01:56 -0400

    using local docker fastapi image

--- main.tf
+++ main.tf
@@ -23,21 +23,32 @@ provider "docker" {
 # Requires two strings before the block: resource type ("docker_image"), and resource name ("nginx")
 # Used to create ID for docker image: docker_image.nginx
 # Can take arguments such as machine sizes, disk image names
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
-}
+#resource "docker_image" "nginx" {
+#  name         = "nginx"
+#  keep_locally = false
+#}
 
-# Sets docker image as the image source for "docker_container" resource
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  # searches for anything prepended with 'var.' in the variables.tf file
-  name  = var.container_name
-  hostname = "terraform-docker"
+resource "docker_container" "fastapi" {
+  image = "fastapi_image"
+  name  = "fastapi_container"
 
   # Docker configuration values
   ports {
-    internal = 80
-    external = 8080
+    internal = 8000
+    external = 8000
   }
 }
+
+# Sets docker image as the image source for "docker_container" resource
+#resource "docker_container" "nginx" {
+#  image = docker_image.nginx.image_id
+#  # searches for anything prepended with 'var.' in the variables.tf file
+#  name  = var.container_name
+#  hostname = "terraform-docker"
+#
+#  # Docker configuration values
+#  ports {
+#    internal = 80
+#    external = 8080
+#  }
+#}
